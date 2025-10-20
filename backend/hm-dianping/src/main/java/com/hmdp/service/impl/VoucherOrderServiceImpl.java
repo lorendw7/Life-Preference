8a2e29cbad2495479e49e483c9968c34c0527ebf
lorendw7 <122377808+lorendw7@users.noreply.github.com>
2025-10-20 22:53:40 +0800

    feat: 乐观锁解决优惠卷超卖问题

--- backend/hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ backend/hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -48,10 +48,12 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if(stock < 1) {
             return Result.fail("库存不足");
         }
-        // 扣减库存
+        // 扣减库存, 乐观锁
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)
+                .update();
         if(!success) {
             return Result.fail("库存不足");
         }
